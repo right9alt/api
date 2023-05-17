d3335dec54e98db1586cf9b72b241e423f3508b6
right9alt <right9alt@gmail.com>
2023-05-17 19:13:42 +0300

    feat: add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
+  mount ActionCable.server, at: '/cable'
+  
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
